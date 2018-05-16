ac136908ce20fe1ae957174f2712f437095d2758
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-05-16 10:06:57 +0200

    Sorterer rapporter på når de ble lagd

--- standweb/models/member.rb
+++ standweb/models/member.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Member < Sequel::Model
-  one_to_many :reports
+  one_to_many :reports, order: Sequel.desc(:created_at)
   many_to_many :standups, join_table: :reports
   many_to_many :teams, join_table: :memberships
 end
