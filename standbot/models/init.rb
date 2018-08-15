f5d162451104487d6dedf298cd135ea482f7e44e
Kyrre Havik Eriksen <kyrremann@gmail.com>
2018-08-15 11:26:15 +0200

    Hindrer dobbeldeklarasjon

--- standbot/models/init.rb
+++ standbot/models/init.rb
@@ -5,13 +5,13 @@ require 'sequel'
 Sequel::Model.plugin :timestamps
 
 if ENV['RACK_ENV'] == 'production'
-  DB = Sequel.postgres(user:     ENV['POSTGRES_USER'],
+  DB ||= Sequel.postgres(user:     ENV['POSTGRES_USER'],
                        password: ENV['POSTGRES_PASSWORD'],
                        database: ENV['POSTGRES_DATABASE'],
                        host:     ENV['POSTGRES_SOCKET_PATH'])
 else
   url = ENV['DATABASE_URL'] ? ENV['DATABASE_URL'] : 'postgres://postgres:postgres@localhost:5432/standbot'
-  DB = Sequel.connect(url)
+  DB ||= Sequel.connect(url)
 end
 
 require_relative 'member'
